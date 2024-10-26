f805bb99477e51355d8570945af1e435e2e526b4
Elena Cores <elenayunxin@gmail.com>
2024-10-26 19:12:46 +0200

    console info OK

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const http = require('http');
+const os = require('os');
+const dotenv = require('dotenv');
+dotenv.config();
+
+// variables de configuración
 const port = process.env.PORT || 3000;
-const os = require("os");
+const interval = process.env.INTERVAL || 5000; // Intervalo desde archivo .env (en ms)
+
+// info inicial del sistema
+console.log(`Sistema operativo: ${os.type()} ${os.release()}`);
+console.log(`Versión de Node.js: ${process.version}`);
 
 const server = http.createServer((req, res) => {
- res.statusCode = 200;
- res.setHeader('Content-Type', 'text/html');
- res.end('<h1>Hello, World!</h1>');
- os.cpus()
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Hello, World!</h1>');
 });
+
 server.listen(port, () => {
  console.log(`Server running at port ${port}`);
 });
+
